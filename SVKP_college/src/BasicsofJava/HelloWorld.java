7f59c5522c54718d72894aed39d973a20ab33016
hemav <hemav@LAPTOP-BH7SCHNS>
2024-10-21 21:59:37 +0530

    2 commit

--- SVKP_college/src/BasicsofJava/HelloWorld.java
+++ SVKP_college/src/BasicsofJava/HelloWorld.java
@@ -2,8 +2,9 @@ package BasicsofJava;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		System.out.println("hello world");
+		
 	}
 
 }
