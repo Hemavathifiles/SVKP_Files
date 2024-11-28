b36f85aadfb39ed3de837c1754f7ab55a07cedbb
hemav <hemav@LAPTOP-BH7SCHNS>
2024-11-28 20:14:33 +0530

    first commit

--- SVKP_college/src/org/tnsif/capgemini/c2tc/Interface/FunctionalInterface1.java
+++
@@ -1,9 +0,0 @@
-package org.tnsif.capgemini.c2tc.Interface;
-import java.lang.FunctionalInterface;
-
-@FunctionalInterface
-public interface FunctionalInterface1 {
-     void sayHello(String name);
-     
-}
-
--- SVKP_college/src/org/tnsif/capgemini/c2tc/Interface/Vehicle.java
+++
@@ -1,9 +0,0 @@
-package org.tnsif.capgemini.c2tc.Interface;
-
-public interface Vehicle {
-		public abstract void start();
-		public abstract void stop();
-		
-	    int Max_Speed=120;
-	  
-}
